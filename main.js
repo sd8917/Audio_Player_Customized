8a096f6f4944e6124b4ab96c5a9650746e47c59b
sd8917 <sudhanshuraj8917@gmail.com>
2021-07-01 00:44:18 +0530

    First version of app

---
+++ main.js
@@ -0,0 +1,40 @@
+console.log("script loaded");
+
+var audioPlayer = document.getElementById("audioplayer")
+var progress = document.getElementById("progress");
+var playBtn = document.getElementById("playbtn")
+var pauseBtn = document.getElementById("pausebtn")
+var volmUp = document.getElementById("vol_inc");
+var volmDown = document.getElementById("vol_dec");
+var slowBtn = document.getElementById("slow");
+var normalBtn = document.getElementById("normal");
+var fastBtn = document.getElementById("fast");
+
+// progress.style.width = 90 + "%" //  Code to increase the width of the progress bar // ;
+
+audioPlayer.ontimeupdate = function() {
+  progress.style.width = ((audioPlayer.currentTime/audioPlayer.duration)*100) + "%";
+  // console.log((audioPlayer.currentTime/audioPlayer.duration)*100);
+};
+// audioPlayer.volume  = 0.2;
+volmUp.addEventListener("click", function(){
+  audioPlayer.volume  = audioPlayer.volume  + 0.2;
+  console.log("volume increased ");
+});
+
+volmDown.addEventListener("click", function(){
+  audioPlayer.volume  = audioPlayer.volume  -  0.2;
+  console.log("volume decreased ");
+  
+})
+playBtn.addEventListener('click', function(){
+  console.log("clicked played !!");
+  audioPlayer.play();
+});
+
+pauseBtn.addEventListener('click', function(){
+  console.log("clicked paused !!");
+  audioPlayer.pause();
+});
+
+
